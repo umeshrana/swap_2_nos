46d4f3b7d1289f1d977095affcaf974d10540a82
Umesh Rana <ur621799@gmail.com>
2021-04-29 09:43:28 +0200

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,30 @@
+# def swap(a,b):
+#     temp = a
+#     a = b
+#     b = temp
+#     return a, b
+# print(swap(3,4))
+
+# a = int(input("Enter a number: "))
+# b = int(input("Enter second number: "))
+# print(a,b)
+# temp = a
+# b = a
+# a = temp
+# print(a,b)
+
+# a = int(input("Enter a number: "))
+# b = int(input("Enter second number: "))
+# print(a,b)
+# a = a+b
+# b = a - b
+# a = a - b
+# print(a,b)
+
+a = int(input("Enter a number: "))
+b = int(input("Enter second number: "))
+print(a,b)
+a = a * b
+b = a / b
+a = a / b
+print(a,b)
